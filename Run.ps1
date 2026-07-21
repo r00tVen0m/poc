9ea8449384a290d7d8ecf1bfe8b24f216ddfbb9d
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-21 14:57:50 +0100

    cc

--- Run.ps1
+++ Run.ps1
@@ -1,1 +1,18 @@
+$Word = "C:\Program Files\Microsoft Office\Office14\WINWORD.EXE"
+$File = "\\FILE01\Users$\emily.davis\Downloads\attachment.docm"
 
+if (Test-Path $File) {
+
+    $WordProcess = Start-Process -FilePath $Word -ArgumentList "`"$File`"" -PassThru
+
+
+    Start-Sleep -Seconds 180
+
+    if (-not $WordProcess.HasExited) {
+        Stop-Process -Id $WordProcess.Id -Force
+    }
+
+    if (Test-Path $File) {
+        Remove-Item $File -Force
+    }
+}
