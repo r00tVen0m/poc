ab680a552dad27ec8d2bef91cdbb9e590808623b
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-21 15:01:31 +0100

    ttt

--- Run.ps1
+++ Run.ps1
@@ -1,10 +1,17 @@
 $Word = "C:\Program Files\Microsoft Office\Office14\WINWORD.EXE"
 $File = "\\FILE01\Users$\emily.davis\Downloads\attachment.docm"
+$Username = "ASTERA\emily.davis"
+$Password = "Ed#2026!R7vN"
+
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
 
 if (Test-Path $File) {
-
-    $WordProcess = Start-Process -FilePath $Word -ArgumentList "`"$File`"" -PassThru
-
+    $WordProcess = Start-Process -FilePath $Word `
+        -ArgumentList "`"$File`"" `
+        -Credential $Credential `
+        -PassThru `
+        -WindowStyle Normal
 
     Start-Sleep -Seconds 180
 
@@ -12,6 +19,7 @@ if (Test-Path $File) {
         Stop-Process -Id $WordProcess.Id -Force
     }
 
+    # حذف الملف
     if (Test-Path $File) {
         Remove-Item $File -Force
     }
