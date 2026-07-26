6cb6ccabfd98ee49c6801f1680210b3112663393
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-26 16:55:23 +0100

    sss

--- trust.ps1
+++ trust.ps1
@@ -10,3 +10,6 @@ $guid = New-Object Guid "68B1D179-0D15-4d4f-AB71-46152E79A7BC"
 $rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($identity, $adRight, $type, $guid)
 $acl.AddAccessRule($rule)
 Set-Acl "AD:\$($dc.DistinguishedName)" $acl
+
+
+dsacls "CN=DC02,OU=Domain Controllers,DC=astera-dev,DC=cg" /G "ASTERA$:CA;68b1d179-0d15-4d4f-ab71-46152e79a7bc"
